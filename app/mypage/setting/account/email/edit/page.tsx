0fd07d0a4d549b5bd42fa3f87a1965619962a0b2
itaikana <2015edcckanaitai@gmail.com>
2026-05-06 12:29:45 +0900

    defaultValuesに初期値を追加

--- app/mypage/setting/account/email/edit/page.tsx
+++ app/mypage/setting/account/email/edit/page.tsx
@@ -34,6 +34,7 @@ export default function Page() {
   } = useForm<changeEmailForm>({
     mode: "onChange",
     defaultValues: {
+      currentEmail:"",
       newEmail: "",
     },
     resolver: zodResolver(changeEmailSchema),
