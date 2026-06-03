0d430e468a08dd8c769cc07f41530bf2e6b171e9
itaikana <2015edcckanaitai@gmail.com>
2026-06-03 17:25:30 +0900

    add: 口コミ投稿の実装　途中

--- app/_libs/schemas/posts.schema.ts
+++ app/_libs/schemas/posts.schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const postSchema = z.object({
+export const postsSchema = z.object({
 
   shopName: z.string()
     .min(1, "店名は必須です"),
@@ -15,15 +15,18 @@ export const postSchema = z.object({
     message: "日付を選択してください",
   }),
 
-  comment: z.string()
-    .min(4, "4文字以上で入力してください")
-    .max(20, "ユーザー名は20文字以内で入力してください"),
+  comment: z.string().optional(),
+    // .min(4, "4文字以上で入力してください")
+    // .max(20, "ユーザー名は20文字以内で入力してください"),
 
-  postImageUrl: z.string().optional(),
+  postsImageUrl: z.string().optional(),
+
+  usageScenes: z.array(z.string())
+    .min(1, "利用シーンを選択してください"),
 
 
 })
 
 // 型を自動生成
-export type PostForm = z.input<typeof postSchema>;
+export type PostsForm = z.input<typeof postsSchema>;
 // export type ProfileFormOutput = z.output<typeof profileSchema>;
--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { supabase } from '@/app/_libs/supabase'
 import { useForm, useFieldArray  } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
@@ -11,9 +10,11 @@ import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
 import { useFetch } from "@/app/_hooks/useFetch";
 import { uploadImage } from "@/app/_libs/uploadImage";
 import { useStorageImage } from "@/app/_hooks/useStorageImage";
-import { postSchema, type PostForm } from "@/app/_libs/schemas/post.schema";
+import { postsSchema, type PostsForm } from "@/app/_libs/schemas/posts.schema";
 import { TextInput } from "@/app/_components/TextInput";
+import { ImageInput } from "@/app/_components/ImageInput";
 import { DateInput } from "@/app/_components/DateInput";
+import { CheckboxInput } from "@/app/_components/CheckboxInput";
 import Label from "@/app/_components/Label";
 import { Button } from "@/app/_components/Button";
 import { BaseSelect } from "@/app/_components/BaseSelect";
@@ -39,23 +40,41 @@ export default function Page() {
       errors,
       isSubmitting,
     }
-  } = useForm<PostForm>({
+  } = useForm<PostsForm>({
     mode: "onChange",
     defaultValues: {
       shopName: "",
       comment: "",
-      postImageUrl:"",
+      postsImageUrl:"",
     },
-    resolver: zodResolver(postSchema),
+    resolver: zodResolver(postsSchema),
   });
 
+  const usageSceneOptions = [
+    {
+      id: "morning",
+      label: "朝ごはん",
+    },
+    {
+      id: "lunch",
+      label: "昼ごはん",
+    },
+    {
+      id: "dinner",
+      label: "夜ごはん",
+    },
+    {
+      id: "break",
+      label: "カフェ・休憩",
+    }
+  ]
 
   // 画像URL取得
-  const postImageKey = watch("postImageUrl");
+  const postsImageKey = watch("postsImageUrl");
 
-  const postImageUrl = useStorageImage({
-    bucket: "profile_thumbnail",
-    imageKey: postImageKey
+  const postsImageUrl = useStorageImage({
+    bucket: "posts_thumbnail",
+    imageKey: postsImageKey
   });
 
 
@@ -76,7 +95,7 @@ export default function Page() {
       })
 
     // RHFに値をセット
-    setValue("postImageUrl", imagePath, {
+    setValue("postsImageUrl", imagePath, {
       shouldDirty: true,
       shouldValidate: true,
     })
@@ -96,7 +115,7 @@ export default function Page() {
       <h2 className='text-3xl font-medium'>口コミ投稿</h2>
 
       <div className='space-y-8 w-full max-w-100'>
-        <div>
+        <div className='posts-bg-primary rounded-[calc(32/16*1rem)] p-6 space-y-4'>
           <h3 className='text-2xl font-medium'>どこで食べましたか？</h3>
           <div className='space-y-2'>
             <Label htmlFor='shopname'>
@@ -114,7 +133,7 @@ export default function Page() {
               </p>
             )}
           </div>
-          <div className='flex items-center justify-center gap-3'>
+          <div className='flex items-center justify-center gap-3 '>
             <div className='space-y-2'>
               <Label htmlFor='visitedDate'>
                   来店日
@@ -146,6 +165,47 @@ export default function Page() {
               )}
             </div>
           </div>
+        </div>
+        <div>
+          <p className='mb-4 text-sm font-medium text-primary'>写真を追加（最大3枚まで）</p>
+            <div className='flex gap-3 items-center justify-between'>
+              {[1, 2, 3].map((index) => (
+                <Label className="cursor-pointer">
+                  <ImageInput
+                    onChange={handleImageChange}
+                  />
+                  <div className='grid w-fit place-items-center gap-2 p-5 border-dashed rounded-[calc(24/16*1rem)] border-2 border-[#DAC2B1] overflow-hidden posts-image-bg-primary'>
+                    <div className='w-fit posts-image-bg-secondary p-2 rounded-full'>
+                      <Image
+                        src={postsImageUrl || "/posts/Icon_addPhoto.svg"}
+                        alt="お店の写真"
+                        width={22}
+                        height={20}
+                        loading='eager'
+                      />
+                    </div>
+                    <p className='text-xs font-bold'>ADD PHOTO</p>
+                  </div>
+                </Label>
+              ))}
+            </div>
+          </div>
+      </div>
+      <div className='posts-bg-primary rounded-[calc(32/16*1rem)] p-6 space-y-4'>
+        <h3 className='text-2xl font-medium'>利用シーン</h3>
+        <div className="flex gap-2 flex-wrap">
+          {usageSceneOptions.map((usageScene) => (
+          <Label className="text-xl font-medium text-primary px-5 py-2.5 input-bg-secondary rounded-full hover:bg-[#A3EED8] hover:text-[#1F6E5D] cursor-pointer" key={usageScene.id}>
+            <CheckboxInput
+              value={usageScene.id}
+              {...register("usageScenes")}
+              className='peer sr-only rounded-full input-bg-secondary'
+              />
+              <span className="inline-block text-xl font-medium text-primary w-full rounded-full input-bg-secondary hover:bg-[#A3EED8] transition-colors peer-checked:bg-[#A3EED8] peer-checked:text-[#1F6E5D]">
+                {usageScene.label}
+              </span>
+          </Label>
+          ))}
         </div>
       </div>
     </div>
